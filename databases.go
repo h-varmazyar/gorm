1898832ef14448293e364ca541b4d9c36d627218
mr_nobody <mr96@jeeb.top>
2021-08-11 10:54:32 -0400

    first commit

---
+++ databases.go
@@ -0,0 +1,82 @@
+package gorm
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"reflect"
+	"strings"
+)
+
+var db *gorm.DB
+var err error
+
+type DatabaseConfig struct {
+	Type     DbType
+	Username string
+	Password string
+	CharSet  string
+	Name     string
+	Host     string
+	Port     int
+	SSLMode  bool
+}
+
+type DbType string
+
+const (
+	MYSQL      DbType = "mysql"
+	SQLITE     DbType = "sqlite"
+	PostgreSQL DbType = "postgre"
+)
+
+func (conf *DatabaseConfig) Initialize(models ...interface{}) (error, *gorm.DB) {
+	switch conf.Type {
+	case MYSQL:
+		dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", conf.Username, conf.Password, conf.Host, conf.Port, conf.Name)
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
+			if strings.Contains(err.Error(), "Unknown database") {
+				log.Info("creating database ", conf.Name)
+				dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", conf.Username, conf.Password, conf.Host, conf.Port, "")
+				db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+				if err == nil {
+					err=db.Exec(fmt.Sprintf("create database %s;", conf.Name)).Error
+				}
+			}
+			return err, nil
+		}
+	case PostgreSQL:
+		ssl := "disable"
+		if conf.SSLMode {
+			ssl = "enable"
+		}
+		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=%s TimeZone=UTC", conf.Host, conf.Username, conf.Password, conf.Name, conf.Port, ssl)
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			return err, nil
+		}
+	case SQLITE:
+		db, err = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	}
+
+	return migrate(models), db
+}
+
+func migrate(models ...interface{}) error {
+	for _, intArr := range models {
+		//fmt.Println("type is:", )
+		fmt.Println(reflect.TypeOf(intArr))
+		for _, model := range intArr.([]interface{}) {
+			if err := db.AutoMigrate(model); err != nil {
+				return err
+			}
+		}
+
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/mrNobody95/gorm
+
+go 1.16
+
+require (
+	github.com/sirupsen/logrus v1.8.1
+	gorm.io/driver/mysql v1.1.2
+	gorm.io/driver/postgres v1.1.0
+	gorm.io/driver/sqlite v1.1.4
+	gorm.io/gorm v1.21.13
+)
